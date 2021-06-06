5a23e3e84fff6c80dc41886ce2a02595e4fc864b
kevinyang15 <kevinyyang15@gmail.com>
2021-06-06 17:28:17 -0300

    commit 1

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -179,6 +179,7 @@ function esPositivo(numero) {
   }
 }
 
+
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
